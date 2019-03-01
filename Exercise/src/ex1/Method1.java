41e6f5f0e3585895a0542a4169f7986d7aecdb17
lenovo <lenovo@DESKTOP-0QCH4M5>
2019-03-01 15:23:02 +0530

    Selenium fb

--- Exercise/src/ex1/Method1.java
+++ Exercise/src/ex1/Method1.java
@@ -11,47 +11,53 @@ import jxl.read.biff.BiffException;
 public class Method1
 {
 	
-	public String method(String str,int a) throws BiffException, IOException
+	public String method(String str,int r, int c) throws BiffException, IOException
 	{
 	
 
 	File f= new File("C:\\Users\\lenovo\\Desktop\\New Folder\\1548395447943Role+.xlsx.xls");
 	Workbook wk=Workbook.getWorkbook(f);
 	Sheet ws=wk.getSheet(0);
-	int r=ws.getRows();
-	int c=ws.getColumns();
+	 r=ws.getRows();
+	c=ws.getColumns();
 	
-	for(int i=1; i<r; i++)
+	for(int i=0; i<r; i++)
 	{
-		for(int j=1; j<c; j++)
+		for(int j=0; j<c; j++)
 		{
 		Cell c1=ws.getCell(j, i);
-		System.out.println(c1.getContents());
+		Cell a;
 		
-	int b=0;
-	String e;
-	
-	while((e=c1.getContents())!=null)
-	{
+		/* String e;
+		int k=0;*/
 		
-		b=b+1;
+		
+		
+			
+			if(ws.getCell(j, i)==a)
+			{
+				System.out.println(c1.getContents());
+				break;
+				
+			
 	
-	if(b==a)
-	
-	{
-		break;
 	
 	}
-	}
 		
-	ws.getClass();
-	System.out.println(a);
-	return e;
+
+
+	
 	}
-	}}
+
+	}
+	return str ;
+	}
+	
+	
 	public static void main(String[] args) throws BiffException, IOException {
 		Method1 obj=new Method1();
-		obj.method("C:\\Users\\\\lenovo\\Desktop\\New Folder\\1548395447943Role+.xlsx.xls", 4);
+		obj.method("C:\\Users\\\\lenovo\\Desktop\\New Folder\\1548395447943Role+.xlsx.xls", 3, 4);
+		
 	}
 }
 	
